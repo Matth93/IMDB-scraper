19185ce14866fc411b2e4668771432f5003cfdd7
Matthijs van Oostende <matthijsvanoostende@hotmail.com>
2020-04-17 12:32:19 +0200

    small corrections, runs properly

--- lib/imdb_scraper.rb
+++ lib/imdb_scraper.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
+require 'CSV'
 require 'open-uri'
 require 'nokogiri'
+# require 'pry-byebug'
 
 def imdb_scraper
   movies_with_link = Hash.new(0)
+  movies_with_budget = Array.new(0)
   url = 'https://www.imdb.com/user/ur36814528/ratings?ref_=nv_usr_rt_4'
 
   # First scrape to get the total amount of movies
@@ -31,8 +34,52 @@ def imdb_scraper
     current_page += 1
   end
 
-  movies_with_link
+  # Search each movie's page for it's budget
+  movies_with_link.each do |movie, link|
+    url = "https://www.imdb.com#{link}"
+    html_content = open(url).read
+    movie_page = Nokogiri::HTML(html_content)
+
+    # Search for the textblock that includes the budget
+    budgetblock = nil
+    textblocks = movie_page.search('#titleDetails .txt-block')
+    textblocks.each do |textblock|
+      budgetblock = textblock.text if textblock.text.include?('Budget')
+    end
+    # Format the budget to include just a numerical value
+    unless budgetblock.nil?
+      stripped_budgetblock = budgetblock.gsub(/\n/, '').strip
+      matchresult = stripped_budgetblock.match(/.{7}\D*(?<budget>\S+)\s+/)
+      formatted_budget = matchresult[:budget].gsub(',', '').to_i
+
+      # Find personal rating, NOTE: not possible yet
+
+      # Add movie budget to a new hash with the name and budget, wich is then
+      # put in another array
+      movies_with_budget.push([movie, formatted_budget])
+      p movie + formatted_budget.to_s
+    end
+  end
+
+  p movies_with_budget
+  return movies_with_budget
 end
 
-p imdb_scraper
-# [#<Nokogiri::XML::Element:0x3fe08fd3f6d0 name="a" attributes=[#<Nokogiri::XML::Attr:0x3fe08fd3f658 name="class" value="flat-button lister-page-next next-page">, #<Nokogiri::XML::Attr:0x3fe08fd3f644 name="href" value="/user/ur36814528/ratings?sort=date_added%2Cdesc&mode=detail&paginationKey=mfq5ijak6z7uymjwuuwsomnsegl34knnqsdztp6xeepepyyfxdfiwpol52uhtjimq3iwclnm7gq7uk2y4kjyazipmv7dko7t7nchy6m47iyfrvleknv4axfhhxudjs5nyx6ild2xqjdqljg6bqac2wheaznk2ouqhjumdrosczr2xdmt25ats7f757neon4q5epni76376m3hnckb4&lastPosition=100">] children=[#<Nokogiri::XML::Text:0x3fe08fd3eff0 "\n                Next\n            ">]>]
+def write_csv_list(movies_with_budget, csv_file)
+  ordered_movies = movies_with_budget.sort { |movie, next_movie| movie[1] <=> next_movie[1] }
+  CSV.open(csv_file, 'wb') do |csv|
+    ordered_movies.each do |movie|
+      csv << movie
+    end
+  end
+  p ordered_movies
+end
+
+# test_array = [["geralds spel", 434344 ],["geest verhalen", 7878878],["viezigheid", 10]]
+
+# write_csv_list(test_array, 'imdb_movies_by_budget.csv')
+write_csv_list(imdb_scraper, 'imdb_movies_by_budget.csv')
+
+# rated_movies_with_budget = [["fefe", 10],["gert", 12],["deh de", 2]]
+# p rated_movies_with_budget.select { |movie| movie[1] = 0 }
+
